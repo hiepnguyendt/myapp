39b44e6c0b7426c7752b69c01410ab97021aadac
hiepnguyendt <89370886+hiepnguyendt@users.noreply.github.com>
2021-12-06 22:38:28 +0700

    update-6-12

--- mongodb.js
+++ mongodb.js
@@ -3,12 +3,66 @@ const MongoClient = require('mongodb').MongoClient
 
 const app = express()
 
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 app.use(express.json())
 var database
+const options ={
+    definition:{
+        openapi: '3.0.0',
+        info : {
+            title: "Node JS API Project for mongoDB",
+            version: '1.0.0'
+        },
+        servers:[
+            {
+                url: 'http://localhost:8080/'
+            }
+        ]
+    },
+    apis: ['./mongodb.js']
+
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+/** *
+ * @swagger
+ * /:
+ *     get:
+ *         summary: AA this api is used to check if get method is working or not  
+ *         description: this api is used to check if get method is working or not
+ *         responses:
+ *             200:
+ *                 description: To test Get method    
+ */
 
 app.get('/', (req, resp) => {
     resp.send('Welcome to mongodb API')
 })
+
+
+/** 
+ * @swagger 
+ * components:
+ *      schema:
+ *          Book:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                  name:
+ *                      type: string
+*/
+
+/** 
+ * @swagger
+ * /api/book:
+ *  get:
+ *      summary: To get all books from mongodb
+ * f
+*/
 
 app.get('/api/book/',(req, resp) =>  {
     database.collection('books').find({}).toArray((err, result) => {
